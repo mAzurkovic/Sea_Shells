22759ba93e98a08a9b27acda903e6fe4155e5d21
mAzurkovic <mattias@zurkovic.com>
2015-10-25 12:25:49 -0700

    misc configs

--- src/main.c
+++ src/main.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <errno.h>
 
 #define MAX_LENGTH 1024
+#define DELIMS " \t\r\n"
 
 int main(int argc, char *argv[]) {
   char line[MAX_LENGTH];
 
   while (1) {
-    printf("$ ");
+    char *sysName = "mattias";
+    printf("%s=> ", sysName);
     if (!fgets(line, MAX_LENGTH, stdin)) break;
     system(line);
   }
