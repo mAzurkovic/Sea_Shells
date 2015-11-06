a74de94bef8d6c3c8fb841e69ea7bf81d98f18cd
mAzurkovic <mattias@zurkovic.com>
2015-11-05 21:41:47 -0800

    change parse command

--- src/main.c
+++ src/main.c
@@ -9,9 +9,40 @@
 int main(int argc, char *argv[]) {
 
    // Execute main subroutine for shell
-   init();
+   // init();
+
+
+    if (strcmp(argv[1], argv[1]) == 0) {
+        system(argv[1]);
+    }
+
+    mainLoop();
 
 }
+
+int mainLoop()
+{
+
+    prompt();
+//    readLine();
+
+}
+
+int prompt()
+{
+
+    while (1)
+    {
+ 
+        char cmd[MAX_LENGTH + 1]; 
+        printf("~> ");
+        scanf("%c", cmd);
+
+       // Read command from standard input, exit on Ctrl+D
+       if(fgets(cmd, sizeof(cmd), stdin) == NULL) break;
+    }
+
+}   
 
 init(void) {
 
@@ -22,15 +53,16 @@ init(void) {
     char *sysName = "Sea#Shells";
     char *cmd;
 
-    printf("%s ~> ", sysName);
+    printf("%s ~> ", sysName); 
     if (!fgets(line, MAX_LENGTH, stdin)) break;
     system(line);
- 
+
     if ((cmd = strtok(line, DELIMS))) {
         errno = 0;
         if (strcmp(cmd, "a") == 0) {
             char *arg = strtok(0, DELIMS);
-            printf("edffffffff\n");    
+            printf("edffffffff\n");   
+            system("ls"); 
         } 
     }
   }
